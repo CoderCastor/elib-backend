472b97ac8989bcd2e02abf505fceb927fbaced91
codercastor <tejasshinde935@gmail.com>
2024-12-25 14:06:15 +0530

    added user router

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import globalErrorHandler from "./middleware/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
-app.get("/", (req, res,next) => {
+app.get("/", (req, res) => {
 
 
   res.json({
     message: "Welcome to Elib APIs",
   });
 });
+
+app.use('/api/users',userRouter)
 
 app.use(globalErrorHandler);
 
